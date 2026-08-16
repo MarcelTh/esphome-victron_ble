0c362cdd28281539f0060ad72a8361b95ad0038f
Marcel Thraenhardt <m.thraenhardt@googlemail.com>
2026-08-16 20:43:11 +0200

    Update victron_listener.cpp
    Logging of Product ID

--- components/victron_scanner/victron_listener.cpp
+++ components/victron_scanner/victron_listener.cpp
@@ -40,6 +40,8 @@ bool VictronListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device)
 
   const auto victron_data = (VICTRON_BT_MANUFACTURER_DATA *) manu_data.data.data();
 
+  ESP_LOGI(TAG, "FOUND Victron device 0x%X,'%s' at %s", victron_data->product_id, device.get_name().c_str(), device.address_str().c_str());
+  
   if (victron_data->product_id == VICTRON_PRODUCT_ID::SMARTSHUNT_500A_50MV) {
     ESP_LOGI(TAG, "FOUND SMART SHUNT 500A/50mV '%s' at %s", device.get_name().c_str(), device.address_str().c_str());
   }
